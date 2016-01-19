a7ed727c7d76b15d3c5b984a9f35a6322ac046b7
ashwinvasudevan <ashwin.techie9396@gmail.com>
2016-01-19 09:34:19 +0530

    Nav Section

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+	$(".incremental-counter").incrementalCounter({
+  "numbers": 3
+});
+
+
+ 	$("#fit ").fitText();
+            //Font-size = 1/10th of the element's width
+        
+
+
+
+
+})(jQuery); // End of use strict
